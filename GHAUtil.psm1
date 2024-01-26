9e526ccd13ac3169cdf143f289a92abc728ebb46
Justin Grote <JustinGrote@users.noreply.github.com>
2024-01-25 19:03:06 -0800

    Try dynamic modulefast action
    Diff module setup
    
    Test
    
    Path fix
    
    Testagain
    
    Testy
    
    Tryrelative again
    
    Test
    
    More cleanup fun
    
    Testytime
    
    Fixup arg passthrough
    
    Revise spec
    
    Remove confirm
    
    Split on all kinds of whitespace
    
    Better split
    
    Wire in step logging and fixup help
    
    Fix debug conditional
    
    Remove dupe passthrough
    
    Check spec formatting
    
    Fix
    
    fixup
    
    Remove nulls
    
    Fixup
    
    Filter nulls
    
    Remove args as test
    
    Test
    
    Try params again
    
    Try explicit params
    
    Test
    
    Commas can be used in nuget syntax dummy

---
+++ GHAUtil.psm1
@@ -0,0 +1,88 @@
+using namespace System.Collections.Generic
+using namespace Microsoft.PowerShell.Commands.Internal.Format
+
+$SCRIPT:StepOutput = $env:GITHUB_STEP_SUMMARY
+
+#Inspired by: https://gist.github.com/aaroncalderon/09a2833831c0f3a3bb57fe2224963942
+<#
+  .Synopsis
+  Converts PowerShell Objects or Format-Table output to Markdown
+#>
+Function ConvertTo-Markdown {
+  [CmdletBinding()]
+  [OutputType([string])]
+  Param (
+    [Parameter(Mandatory, ValueFromPipeline)][PSObject[]]$inputObject
+  )
+  begin {
+    $inputObjects = [List[object]]::new()
+  }
+  process {
+    $inputObjects.Add($inputObject)
+  }
+  end {
+    [Text.StringBuilder]$result = ''
+    if ($inputObjects[0].GetType().Name -ne 'FormatStartData') {
+      $inputObjects = $inputObjects | Format-Table -AutoSize -GroupBy "$(New-Guid)" # This effectively filters any grouping
+    }
+    if ($inputObjects.GetType().Name -contains 'GroupStartData') {
+      throw [NotSupportedException]'Grouped Tables are not supported'
+    }
+
+    [string[]]$stringify = ($inputObjects | Out-String).split([Environment]::NewLine) | Where-Object length
+
+    #We use the dash line because the line above might have spaces in property names
+    $headerDashLine = $stringify[1]
+    [int[]]$headerIndexes = ($headerDashLine | Select-String -Pattern '-+' -AllMatches).Matches.Index | Where-Object { $_ -ne 0 }
+
+    foreach ($currentLine in $stringify) {
+      #Escapes existing pipes that may exist so they don't break the table
+      $currentLine = $currentLine -replace '\|', '\|'
+
+      # Insert | into the string at the header indexes
+      $i = 0
+      foreach ($index in $headerIndexes) {
+        $currentLine = $currentLine.Insert(($index + $i), ' | ')
+        $i += 3
+      }
+
+      [void]$result.AppendLine("| $currentLine |")
+      continue
+    }
+    return $result.ToString()
+  }
+}
+
+function Initialize-GhaEnvironment ([switch]$Debug) {
+  $GLOBAL:VerbosePreference = 'Continue'
+  if ($Debug) {
+    $GLOBAL:DebugPreference = 'Continue'
+  }
+
+  #Make logs more distinctive
+  $GLOBAL:PSStyle.Formatting.Verbose = $PSStyle.Foreground.Cyan
+  $GLOBAL:PSStyle.Formatting.Debug = $PSStyle.Foreground.Magenta
+  $GLOBAL:ErrorView = 'NormalView'
+  $GLOBAL:ErrorActionPreference = 'Stop'
+
+}
+
+filter Out-GhaTable {
+  begin {
+    [List[Object]]$inputObject = @()
+  }
+  process {
+    $inputObject.Add($PSItem)
+  }
+  end {
+    $inputObject | ConvertTo-Markdown | Write-GHAStepSummary
+  }
+}
+
+filter Write-GhaStepSummary ([Parameter(Mandatory, ValueFromPipeline)]$InputObject, [switch]$NoNewLine) {
+  Out-File -InputObject $InputObject -FilePath $SCRIPT:StepOutput -Append:(!$Reset) -NoNewline:$NoNewLine
+}
+
+function Clear-GhaStepSummary {
+  Remove-Item -Path $SCRIPT:StepOutput -Force -ErrorAction SilentlyContinue
+}
