83ef48287e830a528fed7874992ac87a70beeae1
Justin Grote <JustinGrote@users.noreply.github.com>
2024-01-26 22:01:29 -0800

    Try dynamic modulefast actionDiff module setupTestPath fixTestagainTestyTryrelative againTestMore cleanup funTestytimeFixup arg passthroughRevise specRemove confirmSplit on all kinds of whitespaceBetter splitWire in step logging and fixup helpFix debug conditionalRemove dupe passthroughCheck spec formattingFixfixupRemove nullsFixupFilter nullsRemove args as testTestTry params againTry explicit paramsTestCommas can be used in nuget syntax dummy
    * Try dynamic modulefast action
    
    Diff module setup
    
    Test
    
    Path fix
    
    Testagain
    
    Testy
    
    Tryrelative again
    
    Test
    
    More cleanup fun
    
    Testytime
    
    Fixup arg passthrough
    
    Revise spec
    
    Remove confirm
    
    Split on all kinds of whitespace
    
    Better split
    
    Wire in step logging and fixup help
    
    Fix debug conditional
    
    Remove dupe passthrough
    
    Check spec formatting
    
    Fix
    
    fixup
    
    Remove nulls
    
    Fixup
    
    Filter nulls
    
    Remove args as test
    
    Test
    
    Try params again
    
    Try explicit params
    
    Test
    
    Commas can be used in nuget syntax dummy

---
+++ action.ps1
@@ -0,0 +1,34 @@
+param(
+  $bootstrapParams,
+  $imfParams,
+  $Debug
+)
+Import-Module $PSScriptRoot/GHAUtil.psm1
+Initialize-GhaEnvironment -Debug:$Debug
+
+Write-Debug 'Bootstrapping Modulefast'
+#Bootstrap the ModuleFast module
+. $PSScriptRoot\ModuleFast.ps1 @bootstrapParams
+
+if ($imfParams.Specification -and $imfParams.Path) {
+  throw [ArgumentException]'Cannot specify both a path and a specification. Choose one.'
+}
+
+#Process the GHA input string into an array. This is a workaround for the fact that GitHub Actions doesn't support arrays as inputs
+if ($imfParams.Specification) {
+  $imfParams.Specification = $imfParams.Specification -split '\s+' | Where-Object { $PSItem } | ForEach-Object Trim
+}
+
+$imfCommonParams = @{
+  OutVariable = 'installedModules'
+  PassThru    = $true
+  Debug       = $Debug
+  Verbose     = $true
+}
+
+Install-ModuleFast @imfParams @imfCommonParams @args
+
+if ($installedModules) {
+  '### ModuleFast Installed Modules :rocket:' | Write-GhaStepSummary
+  $installedModules | Out-GhaTable
+}
